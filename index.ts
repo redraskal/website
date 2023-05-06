4051a35a56d969ac88b079f298be91e90da8e1fc
redraskal <ben@ryben.dev>
2023-05-06 15:52:15 -0500

    feat: caching

--- index.ts
+++ index.ts
@@ -1,9 +1,16 @@
-import { Hono } from "hono";
+import { Context, Hono } from "hono";
 import { serveStatic } from "hono/bun";
 
 const app = new Hono();
 
-app.use("/*", serveStatic({ root: "./public/" }));
+app.use("*", async (c: Context, next) => {
+	await next();
+	c.res.headers.append("Cache-Control", "max-age=3600");
+});
+
+app.use("/assets/*", serveStatic({ root: "./" }));
+
+app.get("/", serveStatic({ path: "./assets/index.html" }));
 
 console.log("Hello via Bun!");
 
